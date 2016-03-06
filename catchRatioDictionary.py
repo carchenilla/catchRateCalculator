7464cf8e1765b27887e226a656e5ec897ec77f26
carchenilla <sergiomichiels@gmail.com>
2016-03-06 03:22:24 +0100

    Se importa la libreria urllib.request para abrir las paginas del bulbapedia al vuelo y no tener que descargarse los archivos html
    Se añade el parametro hp tanto al tipo de datos pokemon como al codigo del script de carga de la web. HP es necesario para calcular la prob. de captura
    
    Se castean a int los parametros num y ratio (antes no lo estaban)
    
    Se añaden más comentarios
    
    Se añaden sysos para informar del estado de la carga del diccionario si se descarga desde internet
    
    Se cambia el nombre del modulo Pokemon a pokemon (minusculas)
    
    Como ahora se carga el diccionario desde internet, se suprime el html y por tanto el constructor de CatchRatioDictionary solo incluye un parametro

--- catchRatioDictionary.py
+++ catchRatioDictionary.py
@@ -1,14 +1,15 @@
-from bs4 import BeautifulSoup       #BeautifulSoup to navigate in the HTML file
+from bs4 import BeautifulSoup       #BeautifulSoup
+from urllib.request import urlopen  #to open urls
 import pickle                       #Pickle to store de dictionary of pokemon
-from Pokemon import Pokemon         #Pokemon class, obviously
+from pokemon import Pokemon         #Pokemon class, obviously
 
 class CatchRatioDictionary:
 
-    def __init__(self,path,mode=None):      #mode parameter to check if we load from html or file
-        if mode==None:
+    def __init__(self,path=None):      #mode parameter to check if we load from html or file
+        if path!=None:
             self.__dict=self.__loadDictionaryFromFile(path)
         else:
-            self.__dict=self.__getListfromHtml(path)
+            self.__dict=self.__getListfromHtml()
 
     def getPokemonByName(self,name):        #get pokemon by name
         return self.__dict.get(name)
@@ -21,14 +22,55 @@ class CatchRatioDictionary:
             pickle.dump(self.__dict,fp)
 
 
-    def __getListfromHtml(self,htmlPath):
-        pokemonDictionary={}
-        with open(htmlPath + ".html", "r") as file:
-            soup = BeautifulSoup(file,"lxml")
-            tabla = soup.table.contents[1].contents[1].contents[1].contents
-            for i in range(3,len(tabla),2):
-                poke = Pokemon(tabla[i].th.string.strip(" "),tabla[i].a["title"].strip(" "),tabla[i].find_all("td")[-1].contents[0].strip(" "))
-                pokemonDictionary[poke.getName()]=poke
+    def __getListfromHtml(self):
+        print("Creating data from scratch")
+        print("It may take a while depending on your web connection")
+        print("Go watch some animus or anything")
+        print()
+        counter = 0
+
+        pokemonDictionary={}    #Create dictionary
+        page=urlopen("http://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_catch_rate")   #open URL
+        soup = BeautifulSoup(page.read(),"lxml")            #open html with ratio table
+
+        tabla = soup.table.contents[1].contents[1].contents[1].contents     #navigate to ratio table
+        for i in range(3,len(tabla),2):     #extract from each row name, num, ratio and go to the other page to check the base HP
+            num = int(tabla[i].th.string.strip(" "))
+            name = tabla[i].a["title"].strip(" ")
+            ratio = int(tabla[i].find_all("td")[-1].contents[0].strip(" "))
+
+            print("Found pokemon "+name+". Going to check his base HP")
+            #go check the base HP
+
+            try:
+                page2=urlopen("http://bulbapedia.bulbagarden.net/wiki/"+name.replace(" ","_")+"_%28Pok%C3%A9mon%29")
+                soup2 = BeautifulSoup(page2.read(),"lxml")
+                for tab in soup2.find_all("table"):     #navigate to base stat table
+                    if len(tab.find_all("th"))>=2:
+                        myTag = tab.find_all("th")[1]       #check for table with more than 2 headers
+                        try:
+                            if (myTag["style"]=="background: #FF5959; width: 30px;"):       #try to locate Base HP using style of cell
+                                hp = int(myTag.string.rstrip().strip(" "))
+                                break       #if found, we break out of the loop
+                        except KeyError as err:         #possible exceptions we ignore if the header has no style key
+                            pass
+                        except AttributeError as err:   #possible exceptions we ignore if the header has no string value
+                            pass
+            except UnicodeEncodeError as err:
+                if num==32:         #male nidoran
+                    hp=46
+                elif num==29:      #female nidoran
+                    hp=55
+
+            print("Found base HP of pokemon "+name+". Creating data and adding to dictionary")
+            counter+=1
+            print(str("%.2f" % (100*counter/721))+"% completed")
+            print()
+
+            poke = Pokemon(num,name,ratio,hp)       #create pokemon object
+            pokemonDictionary[poke.getName()]=poke      #add pokemon to dictionary
+
+        print("All Pokemon data were added. Done!")
         return(pokemonDictionary)
 
 
@@ -43,6 +85,9 @@ class CatchRatioDictionary:
 
 
 if __name__=="__main__":        #auxiliar main to store dictionary to file
-    d = CatchRatioDictionary("webpage","html")
+    #d = CatchRatioDictionary("dataDictionary")
+    d = CatchRatioDictionary()
     d.saveDictionaryToFile()
     print(len(d.getPokemonDictionary()))
+    #for p in d.getPokemonDictionary().values():
+    #    print(p.toString())
