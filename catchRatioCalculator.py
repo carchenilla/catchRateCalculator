8b917501e8d327a8b631928868dbaa19258fc8d8
carchenilla <sergiomichiels@gmail.com>
2016-03-07 02:44:37 +0100

    Interfaz gráfica en proceso:
    -Archivo .ui con el xml de la interfaz en PyQt
    -Archivo calculatorGUI.py que contiene la interfaz del archivo xml traducida a python
    -Cambio de "super ball" a "great ball" en catchRatioCalculator.py (esta clase será la principal que utilizaremos para correr la aplicación)

--- catchRatioCalculator.py
+++ catchRatioCalculator.py
@@ -3,7 +3,7 @@
 
 status={"asleep":2.5, "frozen":2.5, "no status":1}
 
-balls={"super ball":1.5, "ultra ball":2, "net ball":3, "quick ball":5, "dive ball":3.5, "repeat ball":3, "dusk ball": 3.5}
+balls={"great ball":1.5, "ultra ball":2, "net ball":3, "quick ball":5, "dive ball":3.5, "repeat ball":3, "dusk ball": 3.5}
 #Remaining balls have ratio 1 or need additional calculous
 
 o_power={1:1.5, 2:2, 3:2.5}
